9e93dfe4b6e37479b6f5cc81842d2505fbfc7bd1
hongyue <2933634892@qq.com>
2026-01-10 22:29:39 +0800

    Add files via upload

--- lib/core/widgets/glass_container.dart
+++ lib/core/widgets/glass_container.dart
@@ -3,40 +3,56 @@ import 'package:flutter/material.dart';
 
 class GlassContainer extends StatelessWidget {
   final Widget child;
-  final double blur;
   final double opacity;
+  final double blur;
   final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
   final BorderRadius? borderRadius;
-  final BoxBorder? border;
+  final double borderWidth;
+  final Color? borderColor;
+  final Color? color;
 
   const GlassContainer({
     super.key,
     required this.child,
-    this.blur = 15.0,
     this.opacity = 0.1,
+    this.blur = 15.0,
     this.padding,
+    this.margin,
     this.borderRadius,
-    this.border,
+    this.borderWidth = 1.0,
+    this.borderColor,
+    this.color,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: borderRadius ?? BorderRadius.circular(20),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
-        child: Container(
-          padding: padding,
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(opacity),
-            borderRadius: borderRadius ?? BorderRadius.circular(20),
-            border: border ??
-                Border.all(
-                  color: Colors.white.withOpacity(0.2),
-                  width: 1.0,
-                ),
+    return Container(
+      margin: margin,
+      child: ClipRRect(
+        borderRadius: borderRadius ?? BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
+          child: Container(
+            padding: padding,
+            decoration: BoxDecoration(
+              color: (color ?? Colors.white).withOpacity(opacity),
+              borderRadius: borderRadius ?? BorderRadius.circular(20),
+              border: Border.all(
+                color: borderColor ?? Colors.white.withOpacity(0.2),
+                width: borderWidth,
+              ),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  (color ?? Colors.white).withOpacity(opacity + 0.05),
+                  (color ?? Colors.white).withOpacity(opacity),
+                ],
+              ),
+            ),
+            child: child,
           ),
-          child: child,
         ),
       ),
     );
