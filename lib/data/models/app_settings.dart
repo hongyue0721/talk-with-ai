0cdf38d26366a08e1bb22635a6a5c1ac02a04f3f
hongyue <2933634892@qq.com>
2026-01-11 14:22:12 +0800

    Add files via upload

--- lib/data/models/app_settings.dart
+++ lib/data/models/app_settings.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:hive/hive.dart';
 
 part 'app_settings.g.dart';
@@ -36,34 +35,3 @@ class AppSettings extends HiveObject {
     this.userAvatarPath,
   });
 }
-=======
-import 'package:hive/hive.dart';
-
-part 'app_settings.g.dart';
-
-@HiveType(typeId: 0)
-class AppSettings extends HiveObject {
-  @HiveField(0)
-  String apiKey;
-
-  @HiveField(1)
-  String baseUrl;
-
-  @HiveField(2)
-  String model;
-
-  @HiveField(3)
-  double temperature;
-
-  @HiveField(4)
-  double fontSize;
-
-  AppSettings({
-    required this.apiKey,
-    required this.baseUrl,
-    required this.model,
-    required this.temperature,
-    required this.fontSize,
-  });
-}
->>>>>>> bab8979a3a7f0e9d4103bf81c095be0ce584c4d5
--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -1,84 +1,3 @@
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import '../data/models/app_settings.dart';
-
-class SettingsProvider extends ChangeNotifier {
-  late Box<AppSettings> _box;
-  late AppSettings _settings;
-
-  AppSettings get settings => _settings;
-
-  SettingsProvider() {
-    _init();
-  }
-
-  void _init() async {
-    // Assuming Hive.initFlutter() and registerAdapter are called in main.dart
-    // and the box is opened there or we ensure it's open here.
-    // Ideally, main.dart should await Hive.openBox('settings').
-    if (Hive.isBoxOpen('settings')) {
-      _box = Hive.box<AppSettings>('settings');
-    } else {
-      _box = await Hive.openBox<AppSettings>('settings');
-    }
-    
-    if (_box.isEmpty) {
-      _settings = AppSettings(
-        apiKey: "",
-        baseUrl: "https://api.openai.com",
-        model: "gpt-3.5-turbo",
-        temperature: 0.7,
-        fontSize: 14.0,
-      );
-      await _box.put('config', _settings);
-    } else {
-      _settings = _box.get('config')!;
-    }
-    notifyListeners();
-  }
-
-  void _save() {
-    _settings.save(); // HiveObject extension method
-    notifyListeners();
-  }
-
-  void updateApiKey(String val) { 
-    _settings.apiKey = val; 
-    _save(); 
-  }
-
-  void updateBaseUrl(String val) { 
-    _settings.baseUrl = val; 
-    _save(); 
-  }
-
-  void updateModel(String val) { 
-    _settings.model = val; 
-    _save(); 
-  }
-
-  void updateTemperature(double val) { 
-    _settings.temperature = val; 
-    _save(); 
-  }
-
-  void updateFontSize(double val) {
-    _settings.fontSize = val;
-    _save();
-  }
-
-  void updateBackgroundImage(String? path) {
-    _settings.backgroundImagePath = path;
-    _save();
-  }
-
-  void updateUserAvatar(String? path) {
-    _settings.userAvatarPath = path;
-    _save();
-  }
-}
-=======
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import '../data/models/app_settings.dart';
@@ -147,5 +66,14 @@ class SettingsProvider extends ChangeNotifier {
     _settings.fontSize = val; 
     _save(); 
   }
+
+  void updateBackgroundImage(String? path) {
+    _settings.backgroundImagePath = path;
+    _save();
+  }
+
+  void updateUserAvatar(String? path) {
+    _settings.userAvatarPath = path;
+    _save();
+  }
 }
->>>>>>> bab8979a3a7f0e9d4103bf81c095be0ce584c4d5
